6e3855ef34783f07448ef10457464db920849046
Jill Forsythe <j.forsythe8@gmail.com>
2016-02-09 22:59:59 +0000

    Ease profile info

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ $(function () {
 
   $('.loader').hide();
 
-  $(button).on('click', function (event) {
+  $('.search').on('click', function (event) {
 
     event.preventDefault();
 
@@ -12,16 +12,16 @@ $(function () {
     var list = '';
     var $grid = $('.grid');
 
-    $('header').css({
-      'height': 'auto',
-      'padding': '1rem',
-    });
 
     $(document).bind("ajaxSend", function(){
      $(".loader").show();
    }).bind("ajaxComplete", function(){
-     $(".loader").hide();
-   });
+     $(".loader").slideUp('slow');
+     $('header').css({
+       'height': 'auto',
+       'padding': '1.5rem',
+     });
+    });
 
     $.ajax({
       dataType: 'jsonp',
@@ -67,6 +67,40 @@ $(function () {
     //  end of search function
 
   });
+
+  // $('.load-more').on('click', function () {
+  //
+  //   event.preventDefault();
+  //
+  //   $.ajax({
+  //     dataType: 'jsonp',
+  //     method: 'GET',
+  //     url: 'https://api.instagram.com/v1/tags/' + hashtag + '/media/recent?count=12&client_id=b8586475183a4ad89a5a0ebd4a36fbc2',
+  //   })
+  //
+  //   .done(function(loadMore) {
+  //     $.each(loadMore.pagination, function (i, el) {
+  //       console.log(loadMore);
+  //       // list += '<li>';
+  //       // list +=   '<a href="' + el.link + '">';
+  //       // list +=     '<img src=' + el.images.standard_resolution.url + '>';
+  //       // list +=   '</a>';
+  //       // list +=   '<div class="image-info">';
+  //       // list +=     '<div class="profile-pic"><img src=' + el.user.profile_picture + '></div>';
+  //       // list +=     '<div class=profile-info>';
+  //       // list +=       '<p>' + el.user.username + '</p>';
+  //       // list +=       '<p>' + el.comments.count;
+  //       // list +=       '<i class="fa fa-comments"></i>';
+  //       // list +=       el.likes.count;
+  //       // list +=       '<i class="fa fa-heart"></i>';
+  //       // list +=       '</p>';
+  //       // list +=     '</div>';
+  //       // list +=    '</div>';
+  //       // list += '</li>';
+  //     });
+  //   });
+  //
+  // });
 
     //  end of document ready
 
